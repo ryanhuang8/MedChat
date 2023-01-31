fa4e7e1b46d7671bbeac371a8f488cc85f66e324
huangr0867 <rhuang53@gmail.com>
2023-01-31 15:23:18 -0500

    login page change

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -59,8 +59,8 @@ class _LoginPageState extends State<LoginPage> {
                                 color: Colors.grey.withOpacity(0.5),
                                 spreadRadius: 5,
                                 blurRadius: 7,
-                                offset:
-                                    Offset(0, 3), // changes position of shadow
+                                offset: const Offset(
+                                    0, 3), // changes position of shadow
                               ),
                             ],
                             borderRadius: BorderRadius.circular(30),
